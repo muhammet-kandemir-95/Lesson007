5f37846f2568a85683cce9c9f5aa82fd721774b5
Muhammet Kandemir <mav.bilisim@gmail.com>
2024-02-20 02:01:37 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,73 @@
 ﻿namespace myjinxin
 {
     using System;
+    using System.Globalization;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Reflection.Metadata;
 
     public class Kata
     {
-        public static int Multiply(int x) => x == 1 ? 1 : x * Multiply(x - 1);
+        public static int[] Sliding(int[] nums, int k)
+        {
+            /*
+            i=0,j=0 z=0
+            i=0,j=1 z=1
+            i=0,j=2  z=2
+            i=1,j=0  z=1
+            i=1,j=1  z=2
+            i=1,j=2  z=3
+            i=2,j=0  z=2
+            i=2,j=1  z=3
+            i=2,j=2  z=4
+            i=3,j=0  z=3
+            i=3,j=1  z=4
+            i=3,j=2  z=5
+
+            */ 
+                for (int i = 0; i < nums.Length-2; i++)
+                {
+                    int [] nums2 = new int [k];
+
+                    for (int j = 0; j < k; j++)
+                    {
+                      nums2[j] = nums[i+j];
+                    }
+                    for (int m = 0; m < nums2.Length; m++)
+                    {
+                        
+                    }
+
+                }
+                
+            //code me
+            return new int[]{ };
+        }
+
         public static void Main(string[] args)
         {
-            int x = 5;
-            Console.WriteLine("Result for {0}: {1}", x, Multiply(x));
+            bool fail = false;
+            (int[] output, int[] p1, int p2)[] tests = new (int[] output, int[] p1, int p2)[]
+            {
+                (output: new[] { 3, 3, 5, 5, 6, 7 }, p1: new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, p2: 3),
+                (output: new[] { 7, 7, 7, 7, 7 }, p1: new[] { -7, -8, 7, 5, 7, 1, 6, 0 }, p2: 4),
+                (output: new[] { 7, 4 }, p1: new[] { 7, 2, 4 }, p2: 2),
+                (output: new[] { 11 }, p1: new[] { 9, 11 }, p2: 2),
+                (output: new[] { 9, 11, 12 }, p1: new[] { 9, 11, 12 }, p2: 1),
+                (output: new[] { -1 }, p1: new[] { -1, -2, -3 }, p2: 3),
+                (output: new[] { -1, -2, -3 }, p1: new[] { -1, -2, -3 }, p2: 1),
+                (output: new int[0], p1: new[] { -1, -2, -3 }, p2: 5)
+            };
+            if (tests.Any(o => string.Join(",", o.output) != string.Join(",", Sliding(o.p1, o.p2))))
+            {
+                var r = tests.Where(o => string.Join(",", o.output) != string.Join(",", Sliding(o.p1, o.p2))).First();
+
+                Console.WriteLine("It fails for Sliding(nums=[{0}], k={1}) = [{2}] but expected: [{3}]!", string.Join(", ", r.p1),r.p2, string.Join(", ", Sliding(r.p1, r.p2)), string.Join(", ", r.output));
+            }
+            else
+            {
+                Console.WriteLine("It works!");
+            }
         }
     }
 }
---
+++ Program_Hamza.cs
@@ -0,0 +1,72 @@
+﻿// https://www.codewars.com/kata/58c2158ec7df54a39d00015c/
+
+namespace myjinxin
+{
+    using System;
+    using System.Linq;
+    using System.Reflection.Metadata;
+
+    public class Kata_Hamza
+    {
+        public static bool[] MomentOfTimeInSpace(string moment)
+        {
+            int[] number = new int [moment.Length];
+           
+            int space = 0;
+            for(int i = 0; i < moment.Length; i++)
+            {
+                if(char.IsDigit(moment[i]) == true && moment[i] != '0' )
+                {
+                    number[i] = Convert.ToInt32(moment[i].ToString());
+                }
+                else
+                {
+                    space = space+1;
+                }
+            }
+            int time = 0;
+            for(int i = 0; i < number.Length; i++)
+            {
+                time = time + number[i];
+            }
+            if(time > space)
+            {
+                return new bool[] { false, false, true };
+            }
+            else if(space > time)
+            {
+                return new bool[] { true, false, false };
+            }
+            else
+            {
+                return new bool[] { false, true, false };
+            }
+            
+            
+        }
+
+        public static void Main_Hamza(string[] args)
+        {
+            bool fail = false;
+            (bool[] output, string input)[] tests = new (bool[] output, string input)[]
+            {
+                (output: new bool[]{false, false, true},input: "12:30 am"),
+                (output: new bool[]{false, true, false},input: "12:02 pm"),
+                (output: new bool[]{true, false, false},input: "01:00 pm"),
+                (output: new bool[]{false, false, true},input: "11:12 am"),
+                (output: new bool[]{false, false, true},input: "05:20 pm"),
+                (output: new bool[]{false, true, false},input: "04:20 am"),
+            };
+            if (tests.Any(o => string.Join(",", o.output) != string.Join(",", MomentOfTimeInSpace(o.input))))
+            {
+                var r = tests.Where(o => string.Join(",", o.output) != string.Join(",", MomentOfTimeInSpace(o.input))).First();
+                
+                Console.WriteLine("It fails for '{0}' = [{1}] but expected: [{2}]!", r.input, string.Join(", ", MomentOfTimeInSpace(r.input)), string.Join(", ", r.output));
+            }
+            else
+            {
+                Console.WriteLine("It works!");
+            }
+        }
+    }
+}
