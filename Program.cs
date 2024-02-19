29a93ba2e9d81211f9eafb500add91fdb558d486
Muhammet Kandemir <mav.bilisim@gmail.com>
2024-02-20 00:46:30 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public static int Multiply(int x) => x == 1 ? 1 : x * Multiply(x - 1);
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine("Result for {0}: {1}", x, Multiply(x));
+        }
+    }
+}
